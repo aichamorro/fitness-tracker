89edda8271020e45daaeeb6fc72db9d4de52becb
Alberto Chamorro <alberto.chamorro@hudl.com>
2017-02-01 12:00:24 +0000

    Adds the Metric Graph feature to FitnessTracker
    By accessing the metric historical data, the user will see a graph of the last 7 values for that specific metric. Note that when there is no variation in the y Axis between different samples, the graph will be shown as empty.

---
+++ Clients/Apple/FitnessTracker/FitnessTracker/VIP/GraphHistoryVIP.swift
@@ -0,0 +1,59 @@
+//
+//  GraphHistoryVIP.swift
+//  FitnessTracker
+//
+//  Created by Alberto Chamorro on 01/02/2017.
+//  Copyright © 2017 OnsetBits. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol IMetricGraphInteractor {
+    func findRecordsForCurrentWeek(for bodyMetric: BodyMetric) -> Observable<[(NSDate, Double)]>
+}
+
+protocol IMetricGraphView {
+    var rx_loadCurrentWeek: Observable<Void> { get }
+    var rx_graphData: AnyObserver<([Double], [Double])> { get }
+    var selectedMetric: BodyMetric { get }
+}
+
+typealias IMetricGraphPresenter = (IMetricGraphInteractor, IMetricGraphView, DisposeBag) -> Void
+let MetricGraphPresenter: IMetricGraphPresenter = { (interactor, view, disposeBag) in
+    view.rx_loadCurrentWeek
+        .flatMap { interactor.findRecordsForCurrentWeek(for: view.selectedMetric) }
+        .bindNext { info in
+            let calendar = Calendar.current
+            var dates: [Double] = []
+            var readings: [Double] = []
+            
+            _ = info.map { return ($0.0, $0.1) }.reduce([]) { result, current in
+                let day = calendar.component(.day, from:current.0 as Date)
+                dates.append(Double(day))
+                readings.append(Double(current.1))
+                
+                return result
+            }
+            
+            view.rx_graphData.onNext((dates, readings))
+        }.addDisposableTo(disposeBag)
+}
+
+final class MetricGraphInteractor: IMetricGraphInteractor {
+    private let fitnessInfoRepository: IFitnessInfoRepository
+    
+    init(repository: IFitnessInfoRepository) {
+        self.fitnessInfoRepository = repository
+    }
+    
+    func findRecordsForCurrentWeek(for bodyMetric: BodyMetric) -> Observable<[(NSDate, Double)]> {
+        return fitnessInfoRepository
+            .rx_findWeek(ofDay: NSDate())
+            .flatMap { fetched in
+                return Observable.just(fetched.map {
+                    return ($0.date!, $0.value(for: bodyMetric).doubleValue)
+                })
+            }
+    }
+}
